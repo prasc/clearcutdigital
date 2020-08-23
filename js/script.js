e020ea1c8ee58548107406f8319d62d37cc96d66
prashathc <prashathc@gmail.com>
2020-08-23 19:04:10 -0400

    added some crazy effects

--- js/script.js
+++ js/script.js
@@ -1,21 +1,66 @@
-// Header Text
+// First header Text
+
+// anime
+//   .timeline({ loop: true })
+//   .add({
+//     targets: ".ml15 .word",
+//     scale: [14, 1],
+//     opacity: [0, 1],
+//     easing: "easeOutCirc",
+//     duration: 800,
+//     delay: (el, i) => 800 * i,
+//   })
+//   .add({
+//     targets: ".ml15",
+//     opacity: 0,
+//     duration: 1000,
+//     easing: "easeOutExpo",
+//     delay: 5000,
+//   });
+
+// Second header
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector(".ml11 .letters");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /([^\x00-\x80]|\w)/g,
+  "<span class='letter'>$&</span>"
+);
 
 anime
   .timeline({ loop: true })
   .add({
-    targets: ".ml15 .word",
-    scale: [14, 1],
-    opacity: [0, 1],
-    easing: "easeOutCirc",
+    targets: ".ml11 .line",
+    scaleY: [0, 1],
+    opacity: [0.5, 1],
+    easing: "easeOutExpo",
     duration: 800,
-    delay: (el, i) => 800 * i,
   })
   .add({
-    targets: ".ml15",
+    targets: ".ml11 .line",
+    translateX: [
+      0,
+      document.querySelector(".ml11 .letters").getBoundingClientRect().width +
+        10,
+    ],
+    easing: "easeOutExpo",
+    duration: 800,
+    delay: 100,
+  })
+  .add({
+    targets: ".ml11 .letter",
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: "-=775",
+    delay: (el, i) => 34 * (i + 1),
+  })
+  .add({
+    targets: ".ml11",
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
-    delay: 5000,
+    delay: 1000,
   });
 
 // FAQ
