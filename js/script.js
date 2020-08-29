8426cbf959a91ece9d4aafaad77311d07f3e7930
prashathc <prashathc@gmail.com>
2020-08-29 09:13:22 -0400

    modal working

--- js/script.js
+++ js/script.js
@@ -77,3 +77,42 @@ accordionItemHeaders.forEach((accordionItemHeader) => {
     }
   });
 });
+
+// Modal -> both windows for text and email
+
+// Get the modal
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("closeit")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+btn2.onclick = function () {
+  modal2.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+span2.onclick = function () {
+  modal2.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  console.log(event.target);
+  if (event.target == modal2 || event.target == modal) {
+    modal2.style.display = "none";
+    modal.style.display = "none";
+  }
+};
