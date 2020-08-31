e20e6d0f2703237ec93ec2ef2d909561f79233b0
prashathc <prashathc@gmail.com>
2020-08-31 12:53:52 -0400

    lets talk section working on all pages now

--- js/script.js
+++ js/script.js
@@ -9,48 +9,6 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 
 AOS.init();
 
-// -------------->  Header text affect
-var textWrapper = document.querySelector(".ml11 .letters");
-textWrapper.innerHTML = textWrapper.textContent.replace(
-  /([^\x00-\x80]|\w)/g,
-  "<span class='letter'>$&</span>"
-);
-
-anime
-  .timeline({ loop: true })
-  .add({
-    targets: ".ml11 .line",
-    scaleY: [0, 1],
-    opacity: [0.5, 1],
-    easing: "easeOutExpo",
-    duration: 800,
-  })
-  .add({
-    targets: ".ml11 .line",
-    translateX: [
-      0,
-      document.querySelector(".ml11 .letters").getBoundingClientRect().width +
-        10,
-    ],
-    easing: "easeOutExpo",
-    duration: 800,
-    delay: 100,
-  })
-  .add({
-    targets: ".ml11 .letter",
-    opacity: [0, 1],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: "-=775",
-    delay: (el, i) => 34 * (i + 1),
-  })
-  .add({
-    targets: ".ml11",
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000,
-  });
 
 // --------------> FAQ
 
