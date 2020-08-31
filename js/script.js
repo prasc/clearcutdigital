7d6d11e2599c96fe2379d215ba36880be4d2a42f
prashathc <prashathc@gmail.com>
2020-08-31 11:34:30 -0400

    cleaning up js code

--- js/script.js
+++ js/script.js
@@ -60,10 +60,15 @@ const accordionItemHeaders = document.querySelectorAll(
 
 accordionItemHeaders.forEach((accordionItemHeader) => {
   accordionItemHeader.addEventListener("click", (event) => {
-   //  Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+    //  Uncomment in case you only want to allow for the display of only one collapsed item at a time!
 
-    const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
-    if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+    const currentlyActiveAccordionItemHeader = document.querySelector(
+      ".accordion-item-header.active"
+    );
+    if (
+      currentlyActiveAccordionItemHeader &&
+      currentlyActiveAccordionItemHeader !== accordionItemHeader
+    ) {
       currentlyActiveAccordionItemHeader.classList.toggle("active");
       currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
     }
@@ -78,7 +83,7 @@ accordionItemHeaders.forEach((accordionItemHeader) => {
   });
 });
 
-//  -------------->  Modal 
+//  -------------->  Modal
 
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -96,25 +101,35 @@ var span2 = document.getElementsByClassName("closeit")[0];
 
 // When the user clicks the button, open the modal
 btn.onclick = function () {
-  modal.style.display = "block";
+  modal.classList.add("displayIt");
+  // modal.style.display = "block";
 };
 btn2.onclick = function () {
-  modal2.style.display = "block";
+  modal2.classList.add("displayIt");
+  // modal2.style.display = "block";
 };
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-  modal.style.display = "none";
+  modal.classList.remove("displayIt");
+  // modal.style.display = "none";
 };
 span2.onclick = function () {
-  modal2.style.display = "none";
+  modal2.classList.remove("displayIt");
+  // modal2.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal2 || event.target == modal || event.target.parentElement.parentElement == mobileNav ) {
-    modal2.style.display = "none";
-    modal.style.display = "none";
+  if (
+    event.target == modal2 ||
+    event.target == modal ||
+    event.target.parentElement.parentElement == mobileNav
+  ) {
+    // modal2.style.display = "none";
+    // modal.style.display = "none";
+    modal.classList.remove("displayIt");
+    modal2.classList.remove("displayIt");
     menuToggle.checked = false;
   }
 };
@@ -123,12 +138,14 @@ window.onclick = function (event) {
 
 var nav = document.getElementsByClassName("top-nav")[0];
 
-window.onscroll = function () { 
+window.onscroll = function () {
   "use strict";
-  if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
+  if (
+    document.body.scrollTop >= 200 ||
+    document.documentElement.scrollTop >= 200
+  ) {
     nav.classList.add("scrolled");
-  } 
-  else {
+  } else {
     nav.classList.remove("scrolled");
   }
 };
